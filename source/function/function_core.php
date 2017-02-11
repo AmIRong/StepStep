396da0a8ff3740aeaabde13fa48137c2cf7de013
huomu <huomu@172.16.24.63>
2017-02-11 19:40:44 +0800

    初始化环境

---
+++ source/function/function_core.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ *      [Discuz!] (C)2001-2099 Comsenz Inc.
+ *      This is NOT a freeware, use is subject to license terms
+ *
+ *      $Id: function_core.php 35335 2015-06-17 01:57:38Z hypowang $
+ */
+
+if(!defined('IN_DISCUZ')) {
+	exit('Access Denied');
+}
+
+define('DISCUZ_CORE_FUNCTION', true);
+function return_bytes($val) {
+    $val = trim($val);
+    $last = strtolower($val{strlen($val)-1});
+    switch($last) {
+        case 'g': $val *= 1024;
+        case 'm': $val *= 1024;
+        case 'k': $val *= 1024;
+    }
+    return $val;
+}
+
+function checkrobot($useragent = '') {
+    static $kw_spiders = array('bot', 'crawl', 'spider' ,'slurp', 'sohu-search', 'lycos', 'robozilla');
+    static $kw_browsers = array('msie', 'netscape', 'opera', 'konqueror', 'mozilla');
+
+    $useragent = strtolower(empty($useragent) ? $_SERVER['HTTP_USER_AGENT'] : $useragent);
+    if(strpos($useragent, 'http://') === false && dstrpos($useragent, $kw_browsers)) return false;
+    if(dstrpos($useragent, $kw_spiders)) return true;
+    return false;
+}
+
+function dstrpos($string, $arr, $returnvalue = false) {
+    if(empty($string)) return false;
+    foreach((array)$arr as $v) {
+        if(strpos($string, $v) !== false) {
+            $return = $returnvalue ? $v : true;
+            return $return;
+        }
+    }
+    return false;
+}
+
+function dhtmlspecialchars($string, $flags = null) {
+    if(is_array($string)) {
+        foreach($string as $key => $val) {
+            $string[$key] = dhtmlspecialchars($val, $flags);
+        }
+    } else {
+        if($flags === null) {
+            $string = str_replace(array('&', '"', '<', '>'), array('&amp;', '&quot;', '&lt;', '&gt;'), $string);
+            if(strpos($string, '&amp;#') !== false) {
+                $string = preg_replace('/&amp;((#(\d{3,5}|x[a-fA-F0-9]{4}));)/', '&\\1', $string);
+            }
+        } else {
+            if(PHP_VERSION < '5.4.0') {
+                $string = htmlspecialchars($string, $flags);
+            } else {
+                if(strtolower(CHARSET) == 'utf-8') {
+                    $charset = 'UTF-8';
+                } else {
+                    $charset = 'ISO-8859-1';
+                }
+                $string = htmlspecialchars($string, $flags, $charset);
+            }
+        }
+    }
+    return $string;
+}
+
+
+
+?>
